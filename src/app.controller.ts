ba64287e9a3ba349498441dbf7a9255a7b156571
YevheniiStasuyk <yevhenii.stasiuk@gmail.com>
2021-11-10 11:46:54 +0200

    fixing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Render, UploadedFile, UseInterceptors } from '@nestjs/common'
+import { Controller, Get, HttpCode, Param, Post, Render, UploadedFile, UseInterceptors } from '@nestjs/common'
 import { FileInterceptor } from '@nestjs/platform-express'
 import { AppService } from './app.service'
 import { Express } from 'express'
@@ -9,18 +9,21 @@ export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get('send')
+  @HttpCode(200)
   @Render('send')
   getSend(): void {
 
   }
 
   @Post('send')
+  @HttpCode(202)
   @UseInterceptors(FileInterceptor('file'))
   async postFile(@UploadedFile() file: Express.Multer.File): Promise<string> {
     return await this.appService.postNew(file)
   }
 
   @Get('status::id')
+  @HttpCode(200)
   async getStatusById(@Param('id') id: number): Promise<string> {
     const res: string = await this.appService.getStatusById(id)
     if (res)
